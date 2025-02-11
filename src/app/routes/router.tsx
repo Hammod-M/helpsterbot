102fdcda3f00d6c8efdd110a3d42fabe998b0b3e
Hammod-M <hammod_13@mail.ru>
2025-02-11 16:14:44 +0500

    Added base page

--- src/app/routes/router.tsx
+++ src/app/routes/router.tsx
@@ -21,60 +21,63 @@ import { Chat } from "@/pages/Chat";
 import Images from "@/pages/Images";
 import { VideoPage } from "@/pages/Video";
 
-export const router = createBrowserRouter([
-   {
-      path: "/",
-      element: <MainLayout />,
-      children: [
-         { index: true, element: <HomePage /> },
-         { path: "pricing", element: <PricingPage /> },
-         {
-            path: "blog",
-            children: [
-               { index: true, element: <BlogPage /> },
-               { path: ":postId", element: <BlogPostPage /> },
-            ],
-         },
-         { path: "images", element: <Images /> },
-         { path: "video", element: <VideoPage /> },
-         { path: "login", element: <AdminSignIn /> },
-         {
-            path: "auth",
-            children: [
-               { path: "signup", element: <SignUp /> },
-               { path: "signin", element: <SignIn /> },
-            ],
-         },
-         {
-            path: "chat",
-            element: (
-               <RequireAuth roles={["user"]}>
-                  <Chat />
-               </RequireAuth>
-            ),
-         },
-         {
-            path: "profile",
-            element: (
-               <RequireAuth roles={["user"]}>
-                  <ProfilePage />
-               </RequireAuth>
-            ),
-         },
-      ],
-   },
-   {
-      path: "/admin",
-      element: (
-         <AdminRequireAuth roles={["admin"]}>
-            <AdminLayout />
-         </AdminRequireAuth>
-      ),
-      children: [
-         { index: true, element: <AdminPage /> },
-         { path: "blog", element: <CreatePost /> },
-         { path: "articles", element: <CreateArticle /> },
-         { path: "pricing", element: <PricingManager /> },
-      ],
-   },
-]);
+export const router = createBrowserRouter(
+   [
+      {
+         path: "/",
+         element: <MainLayout />,
+         children: [
+            { index: true, element: <HomePage /> },
+            { path: "pricing", element: <PricingPage /> },
+            {
+               path: "blog",
+               children: [
+                  { index: true, element: <BlogPage /> },
+                  { path: ":postId", element: <BlogPostPage /> },
+               ],
+            },
+            { path: "images", element: <Images /> },
+            { path: "video", element: <VideoPage /> },
+            { path: "login", element: <AdminSignIn /> },
+            {
+               path: "auth",
+               children: [
+                  { path: "signup", element: <SignUp /> },
+                  { path: "signin", element: <SignIn /> },
+               ],
+            },
+            {
+               path: "chat",
+               element: (
+                  <RequireAuth roles={["user"]}>
+                     <Chat />
+                  </RequireAuth>
+               ),
+            },
+            {
+               path: "profile",
+               element: (
+                  <RequireAuth roles={["user"]}>
+                     <ProfilePage />
+                  </RequireAuth>
+               ),
+            },
+         ],
+      },
+      {
+         path: "/admin",
+         element: (
+            <AdminRequireAuth roles={["admin"]}>
+               <AdminLayout />
+            </AdminRequireAuth>
+         ),
+         children: [
+            { index: true, element: <AdminPage /> },
+            { path: "blog", element: <CreatePost /> },
+            { path: "articles", element: <CreateArticle /> },
+            { path: "pricing", element: <PricingManager /> },
+         ],
+      },
+   ],
+   { basename: "/helpsterbot/" }
+);
